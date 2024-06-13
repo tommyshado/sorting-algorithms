f34a0238ae6c875f961b0c79b6d09a548e0a18b7
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-06-13 15:21:32 +0200

    refactored @BubbleSortAlgorithm.ts

--- BubbleSortAlgorithm.ts
+++ BubbleSortAlgorithm.ts
@@ -1,18 +1,11 @@
 export class BubbleSortAlgorithm {
     sortArray(arr: number[]): number[] {
         let arrLength = arr.length;
-        let swapped: boolean;
-
         for (let i = 0; i < arrLength; i++) {
-            swapped = false;
-            for (let j = 0; j < arrLength - i - 1; j++) {
+            for (let j = 0; j < arrLength; j++) {
                 if (arr[j] > arr[j + 1]) {
                     [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-                    swapped = true;
                 }
-            }
-            if (!swapped) {
-                break;
             }
         }
         return arr;
