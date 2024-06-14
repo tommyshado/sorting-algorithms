9517cdf87672da2a59e742e15a481ebedc058b4c
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-06-14 11:25:55 +0200

    Modify BubbleSort

--- BubbleSortAlgorithm.ts
+++ BubbleSortAlgorithm.ts
@@ -1,13 +1,15 @@
-export class BubbleSortAlgorithm {
-    sortArray(arr: number[]): number[] {
-        let arrLength = arr.length;
-        for (let i = 0; i < arrLength; i++) {
-            for (let j = 0; j < arrLength; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-                }
+export function BubbleSort(arr: number[]): number[] {
+    let arrLength = arr.length;
+    for (let i = 0; i < arrLength; i++) {
+        for (let j = 0; j < arrLength - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Keep track of arr[j]
+                const temp: number = arr[j];
+                // Swap the numbers
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        return arr;
     }
+    return arr;
 }
--- test/BubbleSortAlgorithm.test.ts
+++ test/BubbleSortAlgorithm.test.ts
@@ -1,23 +1,21 @@
 import assert from "assert";
-import { BubbleSortAlgorithm } from "../BubbleSortAlgorithm";
-
-const bubbleSort = new BubbleSortAlgorithm();
+import { BubbleSort } from "../BubbleSortAlgorithm";
 
 describe("Bubble Sort Algorithm", () => {
     it("should return an empty array", () => {
-        let sortedArray = bubbleSort.sortArray([]);
+        let sortedArray = BubbleSort([]);
         assert.deepEqual([], sortedArray);
     });
     it("should not sort numbers", () => {
-        let sortedArray = bubbleSort.sortArray([1, 6, 9, 10, 25]);
+        let sortedArray = BubbleSort([1, 6, 9, 10, 25]);
         assert.deepEqual([1, 6, 9, 10, 25], sortedArray);
     });
     it("should sort two numbers", () => {
-        let sortedArray = bubbleSort.sortArray([20, 10])
+        let sortedArray = BubbleSort([20, 10])
         assert.deepEqual([10, 20], sortedArray);
     });
     it("should sort more numbers", () => {
-        let sortedArray = bubbleSort.sortArray([5, -1, -3, 10]);
+        let sortedArray = BubbleSort([5, -1, -3, 10]);
         assert.deepEqual([-3, -1, 5, 10], sortedArray);
     });
 });
